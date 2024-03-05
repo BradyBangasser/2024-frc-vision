0275c642bd659e3862f4baa2473f68adb4ef0c87
Brady Bangasser <bradybbangasser@gmail.com>
2024-03-04 18:43:46 -0600

    rm build

--- build/unsupported/Eigen/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/bardy/Documents/code/robotics/2024-frc-with-samvs/include/simpleNN/eigen/unsupported/Eigen
-# Build directory: /home/bardy/Documents/code/robotics/2024-frc-with-samvs/build/unsupported/Eigen
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("CXX11")
--- build/unsupported/doc/examples/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/bardy/Documents/code/robotics/2024-frc-with-samvs/include/simpleNN/eigen/unsupported/doc/examples
-# Build directory: /home/bardy/Documents/code/robotics/2024-frc-with-samvs/build/unsupported/doc/examples
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
